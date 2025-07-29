fb5d32aadf599178155002bec3a5b83545ca1ffe
LongDiHua <407235991@aliyun.com>
2025-07-29 16:19:44 +0800

    实现session登录

--- hm-dianping/src/main/java/com/hmdp/service/IUserService.java
+++ hm-dianping/src/main/java/com/hmdp/service/IUserService.java
@@ -1,6 +1,8 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 
 import javax.servlet.http.HttpSession;
@@ -16,10 +18,18 @@ import javax.servlet.http.HttpSession;
 public interface IUserService extends IService<User> {
 
     /**
-     * 发
+     * 发送验证码
      * @param phone
      * @param session
      * @return
      */
-    Object sendCode(String phone, HttpSession session);
+    Result sendCode(String phone, HttpSession session);
+
+    /**
+     * 用户登录
+     * @param loginForm
+     * @param session
+     * @return
+     */
+    Object login(LoginFormDTO loginForm, HttpSession session);
 }
--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,23 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.Hutool;
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.log.Log;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -17,4 +30,56 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    /**
+     * 发送验证码
+     * @param phone
+     * @param session
+     * @return
+     */
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //校验手机号格式，不符合返回错误信息
+        if (RegexUtils.isPhoneInvalid(phone)){
+            return Result.fail("手机号格式错误");
+        }
+        //生成验证码
+        String code = RandomUtil.randomNumbers(6);
+        //将验证码保存到session
+        session.setAttribute("code",code);
+        //发送验证码
+        System.out.println("发送验证码成功，验证码:"+code);
+        //返回
+        return Result.ok();
+    }
+
+    /**
+     * 用户登录
+     * @param loginForm
+     * @param session
+     * @return
+     */
+    @Override
+    public Object login(LoginFormDTO loginForm, HttpSession session) {
+        //验证手机号格式，异常则报错
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)){
+            return Result.fail("手机号格式错误");
+        }
+        //校验验证码
+        Object code = session.getAttribute("code");
+        if( code == null || !code.toString().equals(loginForm.getCode())){
+            return Result.fail("验证码错误");
+        }
+        //查询用户手机号，存在则登录，不存在则创建新用户
+        User user = query().eq("phone", phone).one();
+        if (user == null){
+            user = new User();
+            user.setPhone(phone);
+            user.setNickName(USER_NICK_NAME_PREFIX+RandomUtil.randomNumbers(10));
+        }
+        //转化问题，先转换为userDTO再存入session
+        //保存用户到session
+        session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
+        return Result.ok();
+    }
 }
